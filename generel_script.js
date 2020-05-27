bb5a3c962a7057110805ff19ad6fcbfb8c4cae68
ilon0032 <ilon0032@stud.kea.dk>
2020-05-27 10:39:48 +0200

    udkommenteret brugermenu

--- generel_script.js
+++ generel_script.js
@@ -3,11 +3,11 @@
 
      function sidenVises() {
          console.log("siden vises");
-         document.querySelector("#burgermenu").addEventListener("click", burger);
+         //         document.querySelector("#burgermenu").addEventListener("click", burger);
      }
-     // burgermenu animation -------------------------------
-     function burger() {
-         console.log("burgermenu change");
-         this.classList.toggle("burger");
-         document.querySelector("#menu").classList.toggle("hide_nav");
-     }
+     //     // burgermenu animation -------------------------------
+     //     function burger() {
+     //         console.log("burgermenu change");
+     //         this.classList.toggle("burger");
+     //         document.querySelector("#burgermenu").classList.toggle("hide_nav");
+     //     }
